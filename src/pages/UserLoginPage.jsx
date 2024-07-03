dba627030ab70fa98704d343110fc7800534ec8a
Kubilay ADA <kubilayada.26@gmail.com>
2024-07-03 14:47:30 +0200

    styles done

--- src/pages/UserLoginPage.jsx
+++ src/pages/UserLoginPage.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import '../UserLoginPage.css'; // Ensure the CSS file is correctly referenced
 
 const UserLoginPage = () => {
-    return (
-        <div className="login-container">
-            <h1 className="login-title">Welcome to Login Page</h1>
-            <div className="login-form">
-                <input type="email" placeholder="Email" className="login-input" />
-                <input type="password" placeholder="Password" className="login-input" />
-                <button className="login-btn">LOGIN NOW!</button>
-                <button className="register-btn">REGISTER NOW!</button>
+    return ( 
+    <>
+        
+        <div className="contact-container">
+            <h1 className="contact-title">Welcome to</h1>
+            <div className="contact-info">
+                <p><strong>Email:</strong> example@email.com</p>
+                <p><strong>Phone:</strong> +1234567890</p>
+                <button></button>
             </div>
         </div>
-    );
+    </> 
+    
+);
 }
-
+ 
 export default UserLoginPage;
